34e99786a71cdeccf297e5a502baecf60f2210ad
Killer Albertha <killeral@ev-s3.kzn.21-school.ru>
2023-02-25 15:09:28 +0300

    f

--- src/s21_set.cpp
+++ src/s21_set.cpp
@@ -1,7 +1,12 @@
-#include "s21_set.h"
+#include "s21_set.hpp"
 
 #include <iostream>
 #include <set>
 using namespace std;
 using namespace s21;
-int main() { cout << "h" << endl; }
+int main() {
+  set<int> mySet = {1, 2, 3, 10, 4};
+  for (auto i : mySet) {
+    cout << i << endl;
+  }
+}
---
+++ src/s21_set.hpp
@@ -0,0 +1,10 @@
+#ifndef SET_H
+#define SET_H
+
+#include <utility>
+
+#include "avl_tree.h"
+
+namespace s21 {}  // namespace s21
+
+#endif
