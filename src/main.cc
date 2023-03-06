b31c889bdbfb4678dff6fc0fd7fbd3a49d16f2f4
Killer Albertha <killeral@un-k4.kzn.21-school.ru>
2023-03-06 14:35:07 +0300

    Map

--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,9 @@
 using namespace std;
 using namespace s21;
 int main() {
-  map<int, int> myMap;
-  std::cout << myMap.size() << std::endl;
+  map<string, int> myMap;
+  auto res = myMap.insert(make_pair("Armen", 1));
+  std::cout << res.second << std::endl;
   // Set<int> mySet;
   // Set<int> mySet1;
   // std::cout << mySet.size() << std::endl;
