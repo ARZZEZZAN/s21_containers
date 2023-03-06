ca46350c4d208046c87ffb15a4bcaf5df6239f19
Killer Albertha <killeral@un-k4.kzn.21-school.ru>
2023-03-06 13:07:16 +0300

    f

--- src/Map/s21_map.h
+++ src/Map/s21_map.h
@@ -4,6 +4,7 @@
 #include <limits>
 
 #include "../Iterators/Iterator.h"
+#include "../Set/s21_set.tpp"
 
 namespace s21 {
 template <typename T, typename V>
@@ -15,6 +16,34 @@ class Map {
   using constIterator = ConstIterator<T, V>;
   using size_type = size_t;
   using Allocator = std::allocator<T>;
+  using Set = Set<T, T>;
+
+  Map();
+  Map(std::initializer_list<value_type> const& items);
+  Map(const Map& m);
+  Map(Map&& m);
+  ~Map();
+  operator=(Map&& m);
+
+  T& at(const T& key);
+  T& operator[](const T& key);
+
+  iterator begin();
+  iterator end();
+
+  bool empty();
+  size_type size();
+  size_type max_size();
+
+  void clear();
+
+  std::pair<iterator, bool> insert(const value_type& value);
+  std::pair<iterator, bool> insert(const T& key, const T& obj);
+  std::pair<iterator, bool> insert_or_assign(const T& key, const T& obj);
+  void erase(iterator pos);
+  void swap(Map& other);
+  void merge(Map& other);
+  bool contains(const T& key);
 
  private:
 };
--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,10 @@
-#include <set>
+#include <map>
 
 #include "./Map/s21_map.h"
 using namespace std;
 using namespace s21;
 int main() {
+  map<int, int> myMap;
   // Set<int> mySet;
   // Set<int> mySet1;
   // std::cout << mySet.size() << std::endl;
